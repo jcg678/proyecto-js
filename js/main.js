c34495271d9523b4a588ab83f0218f66d6a72e7f
jcg678 <jcg678@hotmail.es>
2019-08-26 19:39:23 +0200

    sobre mi acordeon

--- js/main.js
+++ js/main.js
@@ -1,53 +1,58 @@
 $(document).ready(function(){
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        pager: false
-    });
     
-    var posts = [
-        {
-            title: 'Prueba de titulo 1',
-            date: 'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
-            content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
-        },
-        {
-            title: 'Prueba de titulo 2',
-            date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
-            content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
-        },
-        {
-            title: 'Prueba de titulo 3',
-            date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
-            content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
-        },
-        {
-            title: 'Prueba de titulo 4',
-            date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
-            content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
-        },
-        {
-            title: 'Prueba de titulo 5',
-            date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
-            content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
-        }        
-        
-    ];
+    if(window.location.href.indexOf('index')> -1){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            pager: false
+        });
+    }
+
+    if(window.location.href.indexOf('index')> -1){
+        var posts = [
+            {
+                title: 'Prueba de titulo 1',
+                date: 'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
+                content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
+            },
+            {
+                title: 'Prueba de titulo 2',
+                date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
+                content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
+            },
+            {
+                title: 'Prueba de titulo 3',
+                date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
+                content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
+            },
+            {
+                title: 'Prueba de titulo 4',
+                date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
+                content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
+            },
+            {
+                title: 'Prueba de titulo 5',
+                date:  'Publicado el día: '+ moment().date() + " de "+moment().format("MMMM") +" del "+moment().format("YYYY"),
+                content: "Lorem fistrum quietooor quietooor pecador por la gloria de mi madre. A wan benemeritaar benemeritaar no te digo trigo por no llamarte Rodrigor de la pradera papaar papaar no te digo trigo por no llamarte Rodrigor. Por la gloria de mi madre amatomaa papaar papaar al ataquerl me cago en tus muelas ese que llega me cago en tus muelas amatomaa ese hombree tiene musho peligro a peich."
+            }        
+            
+        ];
     
-    posts.forEach((item, index)=>{
-        var post =`<article class="post">
-		    			<h2>${item.title}</h2>
-		    			<span class="date">${item.date}</span>
-		    			<p>
-                            ${item.content}
-		    			</p>
-		    			<a href="#" class="button-more">Leer más</a>
-		    		</article>`;
-        
-        $("#posts").append(post);
-       
-    });
+        posts.forEach((item, index)=>{
+            var post =`<article class="post">
+    		    			<h2>${item.title}</h2>
+    		    			<span class="date">${item.date}</span>
+    		    			<p>
+                                ${item.content}
+    		    			</p>
+    		    			<a href="#" class="button-more">Leer más</a>
+    		    		</article>`;
+            
+            $("#posts").append(post);
+           
+        });
+    }
     
     var theme= $("#theme");
     $("#to-green").click(function(){
@@ -88,6 +93,10 @@ $(document).ready(function(){
         localStorage.clear();
         window.reload();
     });
+
+    if(window.location.href.indexOf('about')> -1){
+        $('#acordeon').accordion();
+    }
 
 });
 
